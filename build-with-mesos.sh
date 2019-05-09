162a07b5379b2cbc1cd4a350276086ce1acaa710
spafka <spafka@163.com>
2019-05-09 11:00:25 +0800

    init

--- build-with-mesos.sh
+++ build-with-mesos.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-./dev/make-distribution.sh --tgz --mvn mvn  -Phive -Pyarn -Pmesos -Pscala-2.12  -DskipTests
+./dev/make-distribution.sh --tgz --mvn mvn  -Phive -Pyarn -Pmesos  -Phadoop-2.7 -Pscala-2.12  -DskipTests
 
